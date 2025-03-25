08dce6e6339641197f9d9d323636afd39d58ddfc
Ivan <mayhemk57@gmail.com>
2025-03-25 15:20:26 -0600

    updating

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-41sfkm5puotb44zl.us.auth0.com'
+export const clientId = 'GxZ9KhAHSw5WzEOf0LnUQxkKf9vkwLVM'
+export const audience = 'https://dev-41sfkm5puotb44zl.us.auth0.com/api/v2/'
