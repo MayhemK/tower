cdc3fbbd5911075356c0e0928d0c282e9584b6eb
Ivan <mayhemk57@gmail.com>
2025-03-26 17:00:41 -0600

    getting home page styled up

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -4,7 +4,62 @@
 </script>
 
 <template>
-  TItle picture
+  <section class="container-fluid">
+    <div class="row">
+      <div class="col-12 banner align-items-center d-flex">
+        <div class="text-light fs-3 ">
+          <p class="text-decoration-underline fw-bold">Event Management</p>
+          <p>for people, by people</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="container fw-bold fs-3">How Tower Works</div>
+  <section class="container">
+    <div class="row justify-content-center ">
+      <div class="col-4">
+        <div class="card">
+          <div class="container">
+            <div class="row">
+              <div class="col-3 text-success fs-2">🔍</div>
+              <div class="col-9">
+                <p class="fw-bold fs-5">Discover events you are interested in</p>
+                <p>Browse through community hosted events for all the things you love</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="card">
+          <div class="container">
+            <div class="row">
+              <div class="col-3 text-success fs-2">➕</div>
+              <div class="col-9">
+                <p class="fw-bold fs-5">Start and event and invite your friends</p>
+                <p>Create your own Tower event</p>
+                <p class="fs-bold fs-5 text-success" type="button">Create an Event</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="container fw-bold fs-3">Explore top categories</div>
+  <section class="container">
+    <div class="row">
+      <div class="col-6">
+
+      </div>
+    </div>
+  </section>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.banner {
+  background-image: url(https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);
+  background-size: cover;
+  min-height: 60dvh;
+}
+</style>
