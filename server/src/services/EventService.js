343ad1d8329757793025a19188271263830b5431
Ivan <mayhemk57@gmail.com>
2025-03-28 17:05:12 -0600

    end of class friday, need to implement comments now

--- server/src/services/EventService.js
+++ server/src/services/EventService.js
@@ -20,16 +20,16 @@ class EventService {
     return eventToUpdate
   }
   async getEventById(eventId) {
-    const event = await dbContext.Events.findById(eventId).populate('creator', 'name picture')
+    const event = await dbContext.Events.findById(eventId).populate('creator ticketCount', 'name picture')
     return event
   }
   async getAllEvents() {
-    const events = await dbContext.Events.find().populate('creator', ' name picture').populate('ticketCount').sort('-createdAt')
+    const events = await dbContext.Events.find().populate('creator ticketCount', ' name picture').populate('ticketCount').sort('-createdAt')
     return events
   }
   async createEvent(eventData) {
     const event = await dbContext.Events.create(eventData)
-    await event.populate('creator', 'name picture')
+    await event.populate('creator ticketCount', 'name picture')
     return event
   }
 
